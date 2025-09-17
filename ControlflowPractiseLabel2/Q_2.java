72c950b69d6f9a6cd91f252a4b1b5ebcfa41e440
Reema sharma <reemasharma4707@gmail.com>
2025-09-17 11:27:04 +0530

    Add files via upload

---
+++ ControlflowPractiseLabel2/Q_2.java
@@ -0,0 +1,18 @@
+package Control_Flow.Level_2;
+
+import java.util.Scanner;
+
+public class Q_2 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the employee's salary: ");
+        double salary = sc.nextDouble();
+        System.out.print("Enter the years of service: ");
+        int yearsOfService = sc.nextInt();
+        double bonus = 0.0;
+        if (yearsOfService > 5) {
+            bonus = salary * 0.05; 
+        }
+        System.out.println("The bonus amount is: " + bonus);
+    }
+}
---
+++ ControlflowPractiseLabel2/Q_8.java
@@ -0,0 +1,25 @@
+package Control_Flow.Level_2;
+
+import java.util.Scanner;
+
+public class Q_8 {
+	public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter a positive integer: ");
+        int number = sc.nextInt();
+
+        if (number < 1) {
+            System.out.println("The number " + number + " is not a positive integer.");
+        } else {
+            System.out.print("Factors of " + number + " are: ");
+            int counter = 1;
+
+            while (counter <= number) {
+                if (number % counter == 0) {
+                    System.out.print(counter + " ");
+                }
+                counter++;
+            }
+        }
+    }
+}
